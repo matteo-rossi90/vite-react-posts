f05819a96ed3fee2f7e2a65ca50cf1e14b9ec433
matteo-rossi90 <matteo.rossi1314@gmail.com>
2025-02-25 19:52:05 +0100

    feat: set select to filter posts when user has been selected

--- src/App.jsx
+++ src/App.jsx
@@ -6,77 +6,96 @@ import './App.css';
 function App() {
   //const [count, setCount] = useState(0)
   
-  //inserire l'url dell'api relativa ai post
+  //API relative ai post e agli utenti
   const postsUrl = 'https://jsonplaceholder.typicode.com/posts';
 
   const usersUrl = 'https://jsonplaceholder.typicode.com/users';
 
+  //gestione di utenti e post
   const [posts, setPosts] = useState([]);
 
   const [users, setUsers] = useState([]);
 
+  //gestione della selezione di utenti e post
+  const [userSelected, setUserSelected] = useState("");
+
+  const [filteredPosts, setFilteredPosts] = useState([]);
+
+  //creazione dell'endpoint per ricevere i dati dei post
   useEffect(() => {
-    
     const getPosts = async () => {
-      const posts = await fetch(postsUrl)
-        .then(res => res.json())
-        setPosts(posts.slice(0,35))
-        
-    }
-    getPosts()
-    return () => {
-    }
-  }, [])
+      const response = await fetch(postsUrl);
+      const data = await response.json();
+      setPosts(data);
+    };
+    getPosts();
+  }, []);
 
+  //endpoint che riceve i dati degli utenti
   useEffect(() => {
-
     const getUsers = async () => {
-       const users = await fetch(usersUrl)
-         .then(res => res.json())
-       setUsers(users)
-
-     }
-     getUsers()
-     return () => {
-     }
-   }, [])
+      const response = await fetch(usersUrl);
+      const data = await response.json();
+      setUsers(data);
+    };
+    getUsers();
+  }, []);
   
+  useEffect(() => {
+    if (userSelected) {
+      setFilteredPosts(posts.filter((post) => post.userId === userSelected));
+    } else {
+      setFilteredPosts([]);
+    }
+  }, [userSelected, posts]);
+
+  const changeUser = (e) => {
+    setUserSelected(e.target.value ? parseInt(e.target.value) : null || "")
+  }
 
   return (
     <>
       <div>
         <main>
           <h1>PostMania</h1>
-          <form action="#" class="form-container">
+
+          <div className="form-container">
+
+            {/* select che elenca i nomi degli utenti */}
             <div className="input-box">
-              <select name="posts" id="posts">
-                <option value="">Seleziona post</option>
-                {
-                  posts.map(p => <option value={p.id} key={p.id}>{p.title}</option>)
-                }
-                
-              </select>
-              <button class="search">Cerca</button>
-            </div>
-            <div className="input-box">
-              <select name="user" id="user">
+              <select name="user" id="user" onChange={changeUser}>
                 <option value="">Seleziona utente</option>
                 {
                   users.map(u => <option value={u.id} key={u.id}>{u.name}</option>)
                 }
-
               </select>
-              <button class="search">Cerca</button>
+             
+            </div>
+
+            {/* select che elenca i nomi dei post */}
+            <div className="input-box">
+              <select name="posts" id="posts">
+                <option value="">Seleziona post</option>
+                {
+                  filteredPosts.map(p => <option value={p.id} key={p.id}>{p.title}</option>)
+                }
+                
+              </select>
+              
             </div>
             
-          </form>
+          </div>
+
+          {/* cards che presentano i post presenti */}
           <ul className="card-box">
               {
-                posts.map(post =>
+                filteredPosts.map(post =>
                     <li className="post-card" key={post.id}>
                       <header className="post-header">
                         <small>{post.userId} ha pubblicato:</small>
                         <small className="btn-box">
+
+                          {/* React router che rimanda al singolo post */}
                           <Link to={`/post/${post.id}`} className="btn btn-link">
                             <span>Visualizza</span>
                           </Link>
